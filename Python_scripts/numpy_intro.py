56755f3a7ce20d16bbcac0766acd3a4099912c50
Zikri Bayraktar <zikribayraktar@gmail.com>
2016-10-30 23:50:45 -0400

    second update

--- Python_scripts/numpy_intro.py
+++ Python_scripts/numpy_intro.py
@@ -1,4 +1,4 @@
-# This script is for practicing some of the numpy/scipy commands
+# This script is for practicing some of the numpy/scipy/pandas/matplotlib commands
 # Zikri Bayraktar
 
 # import some of the useful modules that we need:
@@ -24,7 +24,7 @@ np.sqrt(newArray3)  #square root func
 np.log(newArray3)   #natural log func
 np.exp(newArray3)   #exponential func
     
-# dot product
+# dot product of vectors
 np.dot(newArray2, newArray3) #dot product
 newArray2.dot(newArray3)  #same dot product
 newArray3.dot(newArray2)  #same dot product
@@ -32,7 +32,26 @@ newArray3.dot(newArray2)  #same dot product
 # norm - ecludian -L2norm
 magn = np.linalg.norm(newArray1)
 
+# random floats in the half-open interval [0.0, 1.0)
+np.random.random(8)  #unifromly distributed random numbers 
 # normally distribured random numbers
 a = np.random.randn(100)
+a.mean()  #mean of the vector a
+a.var()   #variance of the vector a
+
+# create a 2D matrix
+M = np.array([ [1,2], [3,4] ])
+# transpose the matrix
+M.transpose()
+# element-wise multiplication of two matrices
+M*M  #gives element-wise multiplication result
+M.dot(M) #gives matrix-multiplication result
+
+#vector and matrix of zeros/ones
+np.zeros(5)      #vector of zeros
+np.zeros((6,6))  #matrix of zeros
+np.ones(4)       #vector of ones
+np.ones((3,3))   #matrix of ones
 
 
+# end-of-file
