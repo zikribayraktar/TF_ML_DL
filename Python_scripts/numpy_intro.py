f49c35c2c4c78d9c8b673fcaebe57c8bfa8f4bb8
Zikri Bayraktar <zikribayraktar@gmail.com>
2016-11-02 14:25:20 -0400

    third update

--- Python_scripts/numpy_intro.py
+++ Python_scripts/numpy_intro.py
@@ -43,15 +43,63 @@ a.var()   #variance of the vector a
 M = np.array([ [1,2], [3,4] ])
 # transpose the matrix
 M.transpose()
+M.T
 # element-wise multiplication of two matrices
 M*M  #gives element-wise multiplication result
 M.dot(M) #gives matrix-multiplication result
+Minv = np.linalg.inv(M) #gives inverse of matrix
+np.linalg.det(M)  #gives the determinant of matrix
+np.diag(M)  #gives the matrix diagonal
+np.diag([1,5])  #this creates a matrix of zeros with input assigned to diagonals.
+np.trace(M)  #sum of diagonal elements
+np.cov(M.T)  #gives the covariance, note the transpose 
+np.linalg.eig(M)  # gives eigenvalue and eigenvectors
+
+#Solve Ax=b --> inv(A)*Ax = inv(A)*b --> x =inv(A)*b
+a = np.array([ [1,2] , [3,4] ])
+b = np.array([3,4])
+x = np.linalg.inv(a).dot(b)
+x = np.linalg.solve(a,b)  #this is a better method
 
 #vector and matrix of zeros/ones
 np.zeros(5)      #vector of zeros
 np.zeros((6,6))  #matrix of zeros
 np.ones(4)       #vector of ones
 np.ones((3,3))   #matrix of ones
+
+# to read float-data in a CSV file line-by-line
+X=[] #read-into a list
+for line in open("data.csv"):
+    row = line.split(',')
+    val = map(float,row)
+    X.append(val)
+X=np.array(X) #convert list to array
+X.shape  #size of the matrix
+
+
+# PANDAS
+import pandas as pd
+#use pandas to load the data as dataframe
+X = pd.read_csv("data.csv", header=None)
+type(X)  #type of the variable
+X.info()  #summary of the dataframe -- similar to R
+X.head()  #top entries of the dataframe
+# we can convert the dataframe into numpy array
+M = X.as_matrix()
+type(M)  #this will show as numpy.ndarray
+
+#Note the following difference
+# Numpy  returns : X[0] -> 0th row
+# Pandas returns : X[0] -> column that has name 0
+# both are different than Matlab syntax, where numpy might be closest.
+type(X[0])  #returns pandas.core.series.Series
+
+#to access a row in pandas dataframe
+X.iloc[0]  # will return the 0th row of the dataframe
+X.ix[0]   #will also return the 0th row of the dataframe
+type(X.ix[0])  #returns pandas.core.series.Series
+
+
 
 
 # end-of-file
